8dc369570c4cd4f98876301fd3377ad61401c4fa
John Ericson <Ericson2314@Yahoo.com>
2017-02-18 20:00:00 -0500

    ghcHEAD: Build dynamic libs too
    Use DYNAME_GHC_PROGRAMS instead of GhcLibWays to accomplish this.

--- pkgs/development/compilers/ghc/head.nix
+++ pkgs/development/compilers/ghc/head.nix
@@ -50,6 +50,7 @@ in stdenv.mkDerivation (rec {
   #v p dyn
   preConfigure = stdenv.lib.optionalString (buildPlatform != targetPlatform)''
     sed 's|#BuildFlavour  = quick-cross|BuildFlavour  = quick-cross|' mk/build.mk.sample > mk/build.mk
+    echo 'DYNAMIC_GHC_PROGRAMS = YES' >> mk/build.mk
     #echo 'GhcLibWays += p v thr dyn' >> mk/build.mk
     echo 'Stage1Only       = YES' >> mk/build.mk
   '' + stdenv.lib.optionalString androidTarget ''
