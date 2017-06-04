bbb9bf23a2d28ec6de644f63f20018053f3945c2
John Ericson <Ericson2314@Yahoo.com>
2017-06-04 19:15:31 -0400

    lynx: Modernize and fix for cross

--- pkgs/applications/networking/browsers/lynx/default.nix
+++ pkgs/applications/networking/browsers/lynx/default.nix
@@ -1,5 +1,7 @@
-{ stdenv, fetchurl, ncurses, gzip, pkgconfig
+{ stdenv, buildPackages
+, fetchurl, pkgconfig, ncurses, gzip
 , sslSupport ? true, openssl ? null
+, buildPlatform, hostPlatform
 }:
 
 assert sslSupport -> openssl != null;
@@ -13,10 +15,16 @@ stdenv.mkDerivation rec {
     sha256 = "1cqm1i7d209brkrpzaqqf2x951ra3l67dw8x9yg10vz7rpr9441a";
   };
 
-  configureFlags = [ "--enable-widec" ] ++ stdenv.lib.optional sslSupport "--with-ssl";
+  configureFlags = []
+    ++ stdenv.lib.optional sslSupport "--with-ssl=${openssl.dev}"
+    # TODO(@Ericson2314) this option doesn't really have much to do with
+    # cross-compilation, and should probably be controlled separately
+    ++ stdenv.lib.optional (hostPlatform != buildPlatform) "--enable-widec";
 
   nativeBuildInputs = stdenv.lib.optional sslSupport pkgconfig;
-  buildInputs = [ ncurses gzip ] ++ stdenv.lib.optional sslSupport openssl.dev;
+
+  buildInputs = [ ncurses gzip ]
+    ++ stdenv.lib.optional (hostPlatform != buildPlatform) buildPackages.stdenv.cc;
 
   meta = with stdenv.lib; {
     homepage = http://lynx.isc.org/;
