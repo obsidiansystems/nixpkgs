f964822d2da9d2e7f9e4a362059d5cb0ed748750
Ryan Trinkle <ryan.trinkle@gmail.com>
2017-09-21 18:25:46 -0400

    ghc-8.2.1: explicitly set CC when building cross-compiler

--- pkgs/development/compilers/ghc/head.nix
+++ pkgs/development/compilers/ghc/head.nix
@@ -109,6 +109,8 @@ stdenv.mkDerivation rec {
     echo ${rev} >GIT_COMMIT_ID
     ./boot
     sed -i -e 's|-isysroot /Developer/SDKs/MacOSX10.5.sdk||' configure
+  '' + stdenv.lib.optionalString (targetPlatform != hostPlatform) ''
+    export CC="$TARGET_CC"
   '' + stdenv.lib.optionalString (!stdenv.isDarwin) ''
     export NIX_LDFLAGS+=" -rpath $out/lib/ghc-${version}"
   '' + stdenv.lib.optionalString stdenv.isDarwin ''
