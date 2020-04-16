35fc2b79b988267ed1510f11f6de0f263f713dc8
Matthew Bauer <mjbauer95@gmail.com>
2020-04-16 16:32:35 -0500

    [20.09] fetchurl: don’t run ‘postFetch’ on hashed-mirrors
    hashed-mirrors are content addressed. So if $outputHash is in the
    hashed-mirror, changes from ‘postFetch’ would already be made. So,
    running postFetch will end up applying the change /again/, which we
    don’t want.
    
    Only skip postFetch hook if we're not in recursive hash

--- pkgs/build-support/fetchurl/builder.sh
+++ pkgs/build-support/fetchurl/builder.sh
@@ -47,13 +47,20 @@ tryDownload() {
 
 
 finish() {
+    local skipPostFetch="$1"
+
     set +o noglob
 
     if [[ $executable == "1" ]]; then
       chmod +x $downloadedFile
     fi
 
-    runHook postFetch
+    if [[  $outputHashMode == "recursive" || -z "$skipPostFetch" ]]; then
+        runHook postFetch
+    else
+        echo "Skipping postFetch hook for flat hashed-mirror download"
+    fi
+
     exit 0
 }
 
@@ -69,7 +76,13 @@ tryHashedMirrors() {
             --fail --silent --show-error --head "$url" \
             --write-out "%{http_code}" --output /dev/null > code 2> log; then
             tryDownload "$url"
-            if test -n "$success"; then finish; fi
+
+            # We skip postFetch here, because hashed-mirrors are
+            # already content addressed. So if $outputHash is in the
+            # hashed-mirror, changes from ‘postFetch’ would already be
+            # made. So, running postFetch will end up applying the
+            # change /again/, which we don’t want.
+            if test -n "$success"; then finish skipPostFetch; fi
         else
             # Be quiet about 404 errors, which we interpret as the file
             # not being present on this particular mirror.
