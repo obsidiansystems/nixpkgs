830bbc6568815a3202a0f149974a99d1fa44b7e8
Claudio Bley <cbley@exa-online.de>
2020-06-19 12:14:19 +0200

    bazel: 3.2.0 -> 3.3.0
    (cherry picked from commit 21a21a422498c89d2c87ae07e390501318a5a9ea)

--- pkgs/development/tools/bazel-watcher/default.nix
+++ pkgs/development/tools/bazel-watcher/default.nix
@@ -60,7 +60,7 @@ buildBazelPackage rec {
       sed -e '/^FILE:@bazel_gazelle_go_repository_tools.*/d' -i $bazelOut/external/\@*.marker
     '';
 
-    sha256 = "0i77nnbd1sd39qw4vm3n5mwkag3dskqjhzr7qs4w1arbiih45zd4";
+    sha256 = "17vdqs3dpaqc4llmzh4rd8mkm19q192w5gp90jlry3dpwj6b4kdf";
   };
 
   buildAttrs = {
