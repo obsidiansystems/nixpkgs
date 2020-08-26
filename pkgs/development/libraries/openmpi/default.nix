5cf4bba12b14008ed09174f867e286a237dfcf5c
Obsidian <obsidian@obsidian.systems>
2020-08-26 14:06:11 -0400

    openmpi: don't depend on rdma-core

--- pkgs/development/libraries/openmpi/default.nix
+++ pkgs/development/libraries/openmpi/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl, fetchpatch, gfortran, perl, libnl
-, rdma-core, zlib, numactl, libevent, hwloc, pkgsTargetTarget, symlinkJoin
+, zlib, numactl, libevent, hwloc, pkgsTargetTarget, symlinkJoin
 
 # Enable CUDA support
 , cudaSupport ? false, cudatoolkit ? null
@@ -43,8 +43,7 @@ in stdenv.mkDerivation rec {
   buildInputs = with stdenv; [ gfortran zlib ]
     ++ lib.optionals isLinux [ libnl numactl ]
     ++ lib.optionals cudaSupport [ cudatoolkit ]
-    ++ [ libevent hwloc ]
-    ++ lib.optional (isLinux || isFreeBSD) rdma-core;
+    ++ [ libevent hwloc ];
 
   nativeBuildInputs = [ perl ];
 
