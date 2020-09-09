df5441d22d0f59aab65ccc2003a5055f55327270
Obsidian <obsidian@obsidian.systems>
2020-09-09 17:47:24 -0400

    magma: 2.5.0 -> 2.5.3
    It looks like the defasult CMake install phase now works. All of the
    install things are there, so let's just use that.

--- pkgs/development/libraries/science/math/magma/default.nix
+++ pkgs/development/libraries/science/math/magma/default.nix
@@ -2,14 +2,14 @@
 
 with stdenv.lib;
 
-let version = "2.5.0";
+let version = "2.5.3";
 
 in stdenv.mkDerivation {
   pname = "magma";
   inherit version;
   src = fetchurl {
     url = "https://icl.cs.utk.edu/projectsfiles/magma/downloads/magma-${version}.tar.gz";
-    sha256 = "0czspk93cv1fy37zyrrc9k306q4yzfxkhy1y4lj937dx8rz5rm2g";
+    sha256 = "1xjy3irdx0w1zyhvn4x47zni5fwsh6z97xd4yqldz8zrm5lx40n6";
     name = "magma-${version}.tar.gz";
   };
 
@@ -22,24 +22,6 @@ in stdenv.mkDerivation {
   '';
 
   enableParallelBuilding=true;
-  buildFlags = [ "magma" "magma_sparse" ];
-
-  # MAGMA's default CMake setup does not care about installation. So we copy files directly.
-  installPhase = ''
-    mkdir -p $out
-    mkdir -p $out/include
-    mkdir -p $out/lib
-    mkdir -p $out/lib/pkgconfig
-    cp -a ../include/*.h $out/include
-    #cp -a sparse-iter/include/*.h $out/include
-    cp -a lib/*.a $out/lib
-    cat ../lib/pkgconfig/magma.pc.in                   | \
-    sed -e s:@INSTALL_PREFIX@:"$out":          | \
-    sed -e s:@CFLAGS@:"-I$out/include":    | \
-    sed -e s:@LIBS@:"-L$out/lib -lmagma -lmagma_sparse": | \
-    sed -e s:@MAGMA_REQUIRED@::                       \
-        > $out/lib/pkgconfig/magma.pc
-  '';
 
   meta = with stdenv.lib; {
     description = "Matrix Algebra on GPU and Multicore Architectures";
