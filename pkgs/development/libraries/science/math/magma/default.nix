ff4a38734cbaf8a0daf1f547368ecccbe70fb27c
Obsidian <obsidian@obsidian.systems>
2020-09-09 17:55:33 -0400

    magma: support cudatoolkit 11
    This adds a patch from https://github.com/pytorch/builder/pull/461 for
    magma. Also add an option to customize GPU_TARGET. 3.5, 5.0, 5.2, 6.0,
    6.1, 7.0 matches the pytorch default.

--- pkgs/development/libraries/science/math/magma/default.nix
+++ pkgs/development/libraries/science/math/magma/default.nix
@@ -1,4 +1,6 @@
-{ stdenv, fetchurl, cmake, gfortran, cudatoolkit, libpthreadstubs, lapack, blas }:
+{ stdenv, fetchurl, cmake, gfortran, cudatoolkit, libpthreadstubs, lapack, blas, fetchpatch
+, cudaCapabilities ? ["3.5" "5.0" "5.2" "6.0" "6.1" "7.0"]
+}:
 
 with stdenv.lib;
 
@@ -13,7 +15,19 @@ in stdenv.mkDerivation {
     name = "magma-${version}.tar.gz";
   };
 
+  patches = optionals (versionAtLeast (getVersion cudatoolkit) "11") [
+    ./disable-sparse.patch
+    (fetchpatch {
+      url = "https://raw.githubusercontent.com/zasdfgbnm/builder/6f6767ee1ede6f5e8c37958e3521ee954ec67ab9/magma/magma-cuda110/cudaPointerAttributes.patch";
+      sha256 = "14vcmdw1vhrnmj7lk1ik6z492x5xvhzl81w1dw5d1zm8j9pj62pk";
+    })
+  ];
+
   buildInputs = [ gfortran cudatoolkit libpthreadstubs cmake lapack blas ];
+
+  cmakeFlags = [
+    "-DGPU_TARGET=${builtins.concatStringsSep "," (map (s: "sm_${builtins.replaceStrings ["."] [""] s}") cudaCapabilities)}"
+  ];
 
   doCheck = false;
 
