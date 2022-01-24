f38a3be7e4ccdbbb1c140a5afc56acfe2fb92256
John Ericson <John.Ericson@Obsidian.Systems>
2022-01-24 20:07:14 +0000

    darwin: `useAppleSDKLibs` when doing iOS prebuilt, regardless of arch
    This fixes the iOS simulator eval, and also portends well for unifying
    the macOS SDK libs approach with the iOS prebuilt approach long term.

--- pkgs/stdenv/darwin/default.nix
+++ pkgs/stdenv/darwin/default.nix
@@ -41,7 +41,10 @@ assert crossSystem == localSystem;
 let
   inherit (localSystem) system;
 
-  useAppleSDKLibs = localSystem.isAarch64;
+  # Bootstrap version needs to be known to reference headers included in the bootstrap tools
+  bootstrapLlvmVersion = if localSystem.isAarch64 then "11.1.0" else "7.1.0";
+
+  useAppleSDKLibs = localSystem.isAarch64 || localSystem.useiOSPrebuilt;
   haveKRB5 = localSystem.isx86_64;
 
   # final toolchain is injected into llvmPackages_${finalLlvmVersion}
