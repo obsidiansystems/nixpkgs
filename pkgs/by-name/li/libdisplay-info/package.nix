9af51d81cdd4e8f1f24e0ded3e821366f8ec1c76
Artemis Tosini <artemis.tosini@obsidian.systems>
2026-02-18 16:01:21 -0500

    libdisplay-info: fix build for FreeBSD

--- pkgs/by-name/li/libdisplay-info/package.nix
+++ pkgs/by-name/li/libdisplay-info/package.nix
@@ -1,6 +1,7 @@
 {
   lib,
   stdenv,
+  buildPackages,
   fetchFromGitLab,
   meson,
   pkg-config,
@@ -27,9 +28,11 @@ stdenv.mkDerivation (finalAttrs: {
     meson
     pkg-config
     ninja
-    v4l-utils
     hwdata
     python3
+  ] ++ lib.optionals (stdenv.hostPlatform.emulatorAvailable buildPackages) [
+    # Only used for tests, no point in building them if we can't run them
+    v4l-utils
   ];
 
   postPatch = ''
@@ -41,7 +44,7 @@ stdenv.mkDerivation (finalAttrs: {
     mainProgram = "di-edid-decode";
     homepage = "https://gitlab.freedesktop.org/emersion/libdisplay-info";
     license = lib.licenses.mit;
-    platforms = lib.platforms.linux;
+    platforms = lib.platforms.linux ++ lib.platforms.freebsd;
     maintainers = with lib.maintainers; [ pedrohlc ];
   };
 })
